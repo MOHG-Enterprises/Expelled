d587c482d43a67246274753b16d9ce10df5eafa4
caioeax <caio.ea@aluno.ifsc.edu.br>
2026-03-10 22:23:19 -0300

    atualizado mapa

--- mapa/pingpong.tsx
+++ mapa/pingpong.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="pingpong" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="Expelled/mesaDeTenis.png" width="128" height="128"/>
+ <image source="../mesaDeTenis.png" width="128" height="128"/>
 </tileset>
